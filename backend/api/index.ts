9c693ad4555553f11ab0299c69bd0398eacc1a0e
Callum-code-dev <callum@vedaai.co.uk>
2025-11-20 14:06:23 +0000

    Update vercel

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -3,7 +3,7 @@ import { ExpressAdapter } from "@nestjs/platform-express";
 import { AppModule } from "../src/app.module";
 import express, { Express } from "express";
 
-//new push
+//new pushes
 
 let cachedServer: Express;
 
